1d7f23810bd1a5bfe2e4a5e7010273ac745bdda5
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-05-02 00:13:34 +0700

    Create ApiResponse.php

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiResponse
+{
+	public static function success($data = null)
+	{
+		return response()->json([
+			'code' => 200,
+			'data' => $data,
+			'message' => 'Success.'
+		]);
+	}
+
+	public static function dataNotfound()
+	{
+		return response()->json([
+			'code' => 204,
+			'data' => null,
+			'message' => 'Data not found.'
+		]);
+	}
+
+	public static function internalServerError()
+	{
+		return response()->json([
+			'code' => 500,
+			'data' => null,
+			'message' => 'Internal server error.'
+		]);
+	}
+}
